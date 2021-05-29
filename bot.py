336d45429416923e39f9e72727b943b427975dfd
Esteban <estebanmandjukowski@gmail.com>
2021-05-29 20:22:26 +0200

    custom on message available

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from sqlite3 import dbapi2
 from dotenv import load_dotenv
 import discord
 import os
@@ -520,7 +521,6 @@ class Logs(commands.Cog):
     
     @commands.Cog.listener()
     async def on_raw_message_edit(self,payload):
-        print(payload)
         guild_id = int(payload.data["guild_id"])
         log_channel_id = await self.get_logs_channels(guild_id)
         if log_channel_id:
@@ -733,6 +733,73 @@ class CustomPrefixes(commands.Cog):
             await db.commit()
         await ctx.send(f"Custom prefixes edited. You can now use : {custom_prefixes} and of course '$' !")
         
+class CustomOnMessage(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_message(self,message):
+        table = f"_{message.guild.id}"
+        if message.author == self.bot.user:
+            return
+        async with aiosqlite.connect("databases/custom_on_message.db") as db:
+            cursor = await db.execute(f"SELECT description FROM {table} WHERE message_name = ?",(message.content,))
+            result = await cursor.fetchone()
+            if result:
+                await message.channel.send(result[0])
+
+    @commands.Cog.listener()
+    async def on_guild_join(self,guild:discord.Guild):
+        async with aiosqlite.connect("databases/custom_on_message.db") as db:
+            await db.execute(f"CREATE TABLE _{guild.id}(message_name TEXT,description TEXT);")
+            await db.commit()
+    
+    @commands.Cog.listener()
+    async def on_guild_remove(self,guild):
+        async with aiosqlite.connect("databases/custom_on_message.db") as db:
+            await db.execute(f"DROP TABLE _{guild.id};")
+            await db.commit()  
+    
+    @commands.group(aliases=["COM","com"])
+    async def CustomOnMessage(self,ctx):
+        if ctx.invoked_subcommand is None:
+            return await ctx.send("Subcommand required.")
+    
+    @CustomOnMessage.command()
+    async def add(self,ctx,trigger,*,message):
+        guild_id = f"_{ctx.guild.id}"
+        async with aiosqlite.connect("databases/custom_on_message.db") as db:
+            cursor = await db.execute(f"SELECT message_name,description FROM {guild_id} WHERE message_name = ?",(trigger,))
+            check_result = await cursor.fetchone()
+            if check_result:
+                return await ctx.send(f"'{trigger}' already calls an other message ! Pick another name (this is case sensitive).")
+            await db.execute(f"INSERT INTO {guild_id} VALUES(?,?)",(trigger,message))
+            await db.commit()
+        await ctx.send(f"Got it ! If anyone says '{trigger}', I will answer '{message}'.")
+    
+    @CustomOnMessage.command()
+    async def remove(self,ctx,trigger):
+        guild_id = f"_{ctx.guild.id}"
+        async with aiosqlite.connect("databases/custom_on_message.db") as db:
+            cursor = await db.execute(f"SELECT message_name,description FROM {guild_id} WHERE message_name = ?",(trigger,))
+            check_result = await cursor.fetchone()
+            if not check_result:
+                return await ctx.send("Uhm. Actually, this message doesn't call any message from me. Can't remove something that doesn't exist, right ?")
+            await db.execute(f"DELETE FROM {guild_id} WHERE message_name = ?",(trigger,))
+            await db.commit()
+        await ctx.send(f"Got it. I won't answer to '{trigger}' anymore !")
+
+    @CustomOnMessage.command()
+    async def edit(self,ctx,trigger,*,message):
+        guild_id = f"_{ctx.guild.id}"
+        async with aiosqlite.connect("databases/custom_on_message.db") as db:
+            cursor = await db.execute(f"SELECT message_name,description FROM {guild_id} WHERE message_name = ?",(trigger,))
+            check_result = await cursor.fetchone()
+            if not check_result:
+                return await ctx.send("Uhm. Actually, this message doesn't call any message from me. Can't edit something that doesn't exist, right ?")
+            await db.execute(f"UPDATE {guild_id} SET description = ? WHERE message_name = ?",(message,trigger))
+            await db.commit()
+        await ctx.send(f"Just edited what '{trigger}' calls. Now calls '{message}' ! ")
 
 class OwnerOnly(commands.Cog):
     def __init__(self,bot):
@@ -778,6 +845,7 @@ bot.add_cog(Poll(bot))
 bot.add_cog(Logs(bot))
 bot.add_cog(CustomPrefixes(bot))
 bot.add_cog(OwnerOnly(bot))
+bot.add_cog(CustomOnMessage(bot))
 
 bot.run(TOKEN)
 
