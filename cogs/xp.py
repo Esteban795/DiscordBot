9311af5e7f69c7196a417c4b5800e6f1fffa1448
Esteban <estebanmandjukowski@gmail.com>
2021-07-17 16:53:09 +0200

    reworked xp system due to database rework

--- cogs/xp.py
+++ cogs/xp.py
@@ -1,13 +1,14 @@
 from PIL import Image,ImageDraw,ImageFont
 from math import floor,ceil
-import aiosqlite
+import io
 import discord
+from discord.errors import HTTPException
 from discord.ext import commands
 from datetime import datetime
 import functools
-import os
 from bot import get_prefix
 import asyncio
+
 #Suite ag : 40*1.1**n - 30
 class XPSystem(commands.Cog):
     def __init__(self,bot):
@@ -22,25 +23,6 @@ class XPSystem(commands.Cog):
         return bucket.update_rate_limit()
 
     @commands.Cog.listener()
-    async def on_guild_join(self,guild:discord.Guild):
-        async with aiosqlite.connect("databases/xp.db") as db:
-            await db.execute(f"CREATE TABLE _{guild.id}(member_id INT,current_xp INT,next_level_xp INT,current_level INT);")
-            await db.commit()
-    
-    @commands.Cog.listener()
-    async def on_guild_remove(self,guild):
-        async with aiosqlite.connect("databases/xp.db") as db:
-            await db.execute(f"DROP TABLE _{guild.id};")
-            await db.commit() 
-
-    @commands.Cog.listener()
-    async def on_member_remove(self,member):
-        table_name = f"_{member.guild.id}"
-        async with aiosqlite.connect("databases/xp.db") as db:
-            await db.execute(f"DELETE FROM {table_name} WHERE member_id = ?",(member.id,))
-            await db.commit()
-
-    @commands.Cog.listener()
     async def on_message(self,message):
         retry_after = self.ratelimit_check(message)
         guild_prefix = tuple(await get_prefix(self.bot,message))
@@ -48,34 +30,31 @@ class XPSystem(commands.Cog):
             return
         if retry_after is None:
             xp_won = 1 + ceil(min(len(message.content)/100,1) * 8)
-            table_name = f"_{message.guild.id}"
-            async with aiosqlite.connect("databases/xp.db") as db:
-                cursor = await db.execute(f"SELECT current_xp,next_level_xp,current_level FROM {table_name} WHERE member_id = ?",(message.author.id,))
+            async with self.bot.db.execute(f"SELECT current_xp,next_level_xp,current_level FROM xp WHERE member_id = ? and guild_id = ?",(message.author.id,message.guild.id)) as cursor:
                 result = await cursor.fetchone()
-                if result:
-                    current_xp = result[0] + xp_won
-                    next_level_xp = result[1]
-                    current_level = result[2]
-                    if current_xp >= next_level_xp:
-                        while current_xp >= next_level_xp:
-                            current_xp -= next_level_xp
-                            next_level_xp = floor(eval("".join([self.suite1,str(current_level + 1),self.suite2])))
-                            current_level += 1
-                        await db.execute(f"UPDATE {table_name} SET current_xp = ?,next_level_xp = ?,current_level = ? WHERE member_id = ?",(current_xp,next_level_xp,current_level,message.author.id))
-                        await db.commit()
-                        await message.channel.send(f"{message.author.mention} vient de passer au niveau {current_level} ! ")
-                    else:
-                        await db.execute(f"UPDATE {table_name} SET current_xp = ? WHERE member_id = ?",(current_xp,message.author.id))
-                        await db.commit()
+            if result:
+                current_xp = result[0] + xp_won
+                next_level_xp = result[1]
+                current_level = result[2]
+                if current_xp >= next_level_xp:
+                    current_xp -= next_level_xp
+                    next_level_xp = floor(eval("".join([self.suite1,str(current_level + 1),self.suite2])))
+                    current_level += 1
+                    await self.bot.db.execute(f"UPDATE xp SET current_xp = ?,next_level_xp = ?,current_level = ? WHERE member_id = ? and guild_id = ?",(current_xp,next_level_xp,current_level,message.author.id,message.guild.id))
+                    await self.bot.db.commit()
+                    return await message.channel.send(f"{message.author.mention} is now level {current_level} ! ")
                 else:
-                    await db.execute(f"INSERT INTO {table_name} VALUES(?,?,?,0);",(message.author.id,xp_won,self.u0))
-                    await db.commit()
+                    await self.bot.db.execute(f"UPDATE xp SET current_xp = ? WHERE member_id = ? AND guild_id = ?",(current_xp,message.author.id,message.guild.id))
+                    await self.bot.db.commit()
+            else:
+                await self.bot.db.execute("INSERT INTO xp VALUES(?,?,?,?,0);",(message.author.id,message.guild.id,xp_won,self.u0))
+                await self.bot.db.commit()
 
-    def xpcard(self,member_id,member_name,rank,level,xp,xp_level_suivant):
+    def xpcard(self,img,member_name,rank,level,xp,xp_level_suivant):
         FNT_30 = ImageFont.truetype("fonts/universcondensed.ttf", 30)
         FNT_25 = ImageFont.truetype("fonts/universcondensed.ttf", 25)
         FNT_20 = ImageFont.truetype("fonts/universcondensed.ttf", 20)
-        avatar = Image.open(f'avatars/{member_id}_avatar.png').resize((125,125))
+        avatar = Image.open(img).resize((125,125))
 
         background_to_crop = Image.new("L", avatar.size,color=0)
         im_rgba = avatar.copy()
@@ -101,40 +80,45 @@ class XPSystem(commands.Cog):
         d.text((370,50),text=f"Rank : #{rank}",font=FNT_25,fill=(0,0,0))
         d.text((380,100),text=f"{xp}/{xp_level_suivant}xp",font=FNT_25,fill=(0,0,0))
         img.paste(icon,(50,40),icon)
-        img.save(f"avatars/{member_id}_rank_card.png")
+        return img
 
-    async def run_xp_card(self,member_id,member,rank,current_level,current_xp,next_level_xp):
-        t = functools.partial(self.xpcard,member_id,member,rank,current_level,current_xp,next_level_xp)
+    async def run_xp_card(self,ctx,img,name,rank,current_level,current_xp,next_level_xp):
+        t = functools.partial(self.xpcard,img,name,rank,current_level,current_xp,next_level_xp)
         m = await self.bot.loop.run_in_executor(None,t)
+        bytes_io = io.BytesIO()
+        m.save(bytes_io,"PNG",quality=95)
+        bytes_io.seek(0)
+        file = discord.File(fp=bytes_io,filename="rank_card.png")
+        try:
+            await ctx.send(file=file)
+        except HTTPException:
+            return await ctx.send("Something went wrong, I'm investigating on it.")
+        file.close()
+        return
 
     @commands.command()
     async def rank(self,ctx,member:discord.Member=None):
         member = member or ctx.author
-        member_id = member.id
-        table_name = f"_{ctx.guild.id}"
-        async with aiosqlite.connect("databases/xp.db") as db:
-            cursor = await db.execute(f"SELECT current_xp,next_level_xp,current_level FROM {table_name} WHERE member_id = ?",(member_id,))
+        async with self.bot.db.execute(f"SELECT current_xp,next_level_xp,current_level FROM xp WHERE member_id = ? AND guild_id = ?",(member.id,ctx.guild.id)) as cursor:
             result = await cursor.fetchone()
-            if result:
-                await member.avatar_url_as(format="webp").save(f"avatars/{member_id}_avatar.png")
-                await self.run_xp_card(member_id,str(member),10,result[2],result[0],result[1])
-                await ctx.send(file=discord.File(f"avatars/{member_id}_rank_card.png"))
-                os.remove(f"avatars/{member_id}_avatar.png")
-                os.remove(f"avatars/{member_id}_rank_card.png")
-            else:
-                return await ctx.send("This member never spoke in the chat. How scary it is. Or they spoke in a channel I don't have access to. Either way, this ain't cool.")
+        if result:
+            bytes_io = io.BytesIO(await member.avatar_url_as(format="webp").read())
+            await self.run_xp_card(ctx,bytes_io,str(member),10,result[2],result[0],result[1])
+        else:
+            return await ctx.send("This member never spoke in the chat. How scary it is. Or they spoke in a channel I don't have access to. Either way, this ain't cool.")
 
     @commands.command(aliases=["levels"])
     async def ranking(self,ctx):
         l = []
-        table_name = f"_{ctx.guild.id}"
-        async with aiosqlite.connect("databases/xp.db") as db:
-            async with db.execute(f"SELECT member_id,current_level FROM {table_name} ORDER BY current_level DESC,current_xp DESC;") as cursor:
-                async for row in cursor:
-                    member = ctx.guild.get_member(row[0])
-                    l.append(f"• {member}. Level : {row[1]}")
+        count = 1
+        async with self.bot.db.execute("SELECT member_id,current_level FROM xp WHERE guild_id = ? ORDER BY current_level DESC,current_xp DESC;",(ctx.guild.id,)) as cursor:
+            async for row in cursor:
+                member = ctx.guild.get_member(row[0])
+                if member:
+                    l.append(f"{count}.  {member.mention}. Level : {row[1]}")
+                    count += 1
         embed = discord.Embed(title=f"{ctx.guild}'s ranking".capitalize(),color=0x03fcc6,timestamp=datetime.utcnow(),description="\n".join(l))
-        await ctx.send(embed=embed)
+        return await ctx.send(embed=embed)
 
     @commands.group()
     @commands.has_permissions(administrator=True)
@@ -145,31 +129,28 @@ class XPSystem(commands.Cog):
     @reset.command()
     async def all(self,ctx):
         try:
-            await ctx.send("This will delete E.V.E.R.Y existing datas about your rank. No backup possible ! Type 'yes' to continue.")
+            await ctx.send("This will delete E.V.E.R.Y existing datas about people's rank on your server. No backup possible ! Type 'yes' to continue.")
             confirm = await self.bot.wait_for("message",check=lambda m: m.author == ctx.author and m.channel == ctx.channel,timeout=10)
         except asyncio.TimeoutError:
             await ctx.send("You didn't answer fast enough. Aborting mission !")
         else:
             if confirm.content.lower() == "yes":
-                table_name = f"_{ctx.guild.id}"
-                async with aiosqlite.connect("databases/xp.db") as db:
-                    await db.execute(f"DELETE FROM {table_name};")
-                    await db.commit()
+                await self.bot.db.execute(f"DELETE FROM xp WHERE guild_id = ?;",(ctx.guild.id,))
+                await self.bot.db.commit()
     
     @reset.command()
     async def member(self,ctx,member:discord.Member=None):
         member = member or ctx.author
         try:
-            await ctx.send(f"This will reset {member} levels. No backup possible ! Type 'yes' to continue.")
+            await ctx.send(f"This will reset {member.mention} levels. No backup possible ! Type 'yes' to continue.",allowed_mentions=self.bot.no_mentions)
             confirm = await self.bot.wait_for("message",check=lambda m: m.author == ctx.author and m.channel == ctx.channel,timeout=10)
         except asyncio.TimeoutError:
             await ctx.send("You didn't answer fast enough. Aborting mission !")
         else:
             if confirm.content.lower() == "yes":
-                table_name = f"_{ctx.guild.id}"
-                async with aiosqlite.connect("databases/xp.db") as db:
-                    await db.execute(f"DELETE FROM {table_name} WHERE member_id = ?;",(member.id,))
-                    await db.commit()
+                await self.bot.db.execute(f"DELETE FROM xp WHERE member_id = ? AND guild_id = ?;",(member.id,ctx.guild.id))
+                await self.bot.db.commit()
+                return await ctx.send(f"{member.mention} was brought back to 0.",allowed_mentions=self.bot.no_mentions)
 
 def setup(bot):
     bot.add_cog(XPSystem(bot))
