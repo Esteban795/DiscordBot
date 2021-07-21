638f73e79f2bb4397247bcffd863953358275d7c
Esteban <estebanmandjukowski@gmail.com>
2021-07-21 18:52:36 +0200

    added to-do lists

--- cogs/remind.py
+++ cogs/remind.py
@@ -10,6 +10,17 @@ class Reminder(commands.Cog):
         self.bot = bot
         self.reminders_loop.start()
 
+    async def _get_author_to_do_list_id(self,ctx):
+        """Returns the to-do list's ID from a member. If the member doesn't have any to-do list setup, returns None."""
+        async with self.bot.db.execute("SELECT list_id FROM todolists WHERE member_id = ? AND guild_id = ?",(ctx.author.id,ctx.guild.id)) as cursor:
+            result = await cursor.fetchone()
+        return result
+    
+    async def _todo_exists(self,list_id,to_do):
+        async with self.bot.db.execute("SELECT to_do_id FROM todos WHERE list_id = ? AND to_do = ?",(list_id,to_do)) as cursor:
+            result = await cursor.fetchone()
+        return result
+
     @tasks.loop(seconds=30)
     async def reminders_loop(self):
         await self.bot.wait_until_ready()
@@ -35,17 +46,109 @@ class Reminder(commands.Cog):
         await self.bot.db.commit()
         await ctx.send(f"Alright, {ctx.author.mention}, see you soon !")
                     
-    @commands.command()
+    @commands.group(invoke_without_command=True,aliases=["todos"])
     async def todo(self,ctx,todo:typing.Union[int,str]=None):
         if ctx.invoked_subcommand is None:
             if not todo:
-                sql = "SELECT to_do,done FROM todos WHERE list_id = (SELECT list_id FROm todolists WHERE member_id = ? AND guild_id = ?"
+                sql = "SELECT to_do,done,importance FROM todos WHERE list_id = (SELECT list_id FROM todolists WHERE member_id = ? AND guild_id = ?) ORDER BY importance ASC;"
                 l = []
                 async with self.bot.db.execute(sql,(ctx.author.id,ctx.guild.id)) as cursor:
                     async for row in cursor:
-                        emoji = "✓" if row[1] == 1 else "⨯"
-                        l.append(f"• {row[0]} : {emoji}")
+                        emoji = "\U00002705" if row[1] == 1 else "\U0000274c"
+                        l.append(f"{row[2]}.  {row[0]} : {emoji}")
+                if len(l) == 0:
+                    l = ["You don't have any to-dos yet !"]
+                joined = "\n".join(l)
                 em = discord.Embed(title=f"{ctx.author}'s to do list",color=0xaaffaa,timestamp=datetime.datetime.utcnow())
+                em.add_field(name="\u2800",value=f"```md\n{joined}\n```")
+                await ctx.send(embed=em)
+    
+    @todo.command()
+    async def create(self,ctx,*,to_do=None):
+        to_do_list_id = await self._get_author_to_do_list_id(ctx)
+        if to_do_list_id:
+            return await ctx.send("You can only have one to-do list per server.")
+        insert = await self.bot.db.execute_insert("INSERT INTO todolists(member_id,guild_id) VALUES(?,?)",(ctx.author.id,ctx.guild.id))
+        await self.bot.db.commit()
+        to_do_list_id = insert[0]
+        if to_do:
+            await self.bot.db.execute("INSERT INTO todos(list_id,to_do,done,importance) VALUES(?,?,0,1)",(to_do_list_id,to_do))
+            await self.bot.db.commit()
+        return await ctx.send("Your to-do list for this server was created.")
+
+    @todo.command()
+    async def add(self,ctx,*,to_do):
+        to_do_list_id = await self._get_author_to_do_list_id(ctx)
+        if not to_do_list_id:
+            return await ctx.send("You didn't setup a to-do list yet ! Create one using `?todo create <your first to do>` or simply `?todo create`.")
+        to_do_exists = await self._todo_exists(to_do_list_id[0],to_do)
+        if to_do_exists:
+            return await ctx.send("Uhm. This to-do already exists in your list.")
+        await self.bot.db.execute("INSERT INTO todos(list_id,to_do,done,importance) VALUES (?,?,0,1);",(to_do_list_id[0],to_do))
+        await self.bot.db.commit()
+        return await ctx.send(f"Successfully added {to_do} to your to-do list.")
+    
+    @todo.command()
+    async def done(self,ctx,*,to_do):
+        to_do_list_id = await self._get_author_to_do_list_id(ctx)
+        if not to_do_list_id:
+            return await ctx.send("You didn't setup a to-do list yet ! Create one using `?todo create <your first to do>` or simply `?todo create`.")
+        to_do_exists = await self._todo_exists(to_do_list_id[0],to_do)
+        if not to_do_exists:
+            return await ctx.send("Uhm. This to-do doesn't exist.")
+        await self.bot.db.execute("UPDATE todos SET done = 1 WHERE to_do_id = ?",(to_do_exists[0],))
+        await self.bot.db.commit()
+        return await ctx.send(f"{to_do} was set to 'Done'.")
+
+    @todo.command()
+    async def remove(self,ctx,*,to_do):
+        to_do_list_id = await self._get_author_to_do_list_id(ctx)
+        if not to_do_list_id:
+            return await ctx.send("You didn't setup a to-do list yet ! Create one using `?todo create <your first to do>` or simply `?todo create`.")
+        to_do_exists = await self._todo_exists(to_do_list_id[0],to_do)
+        if not to_do_exists:
+            return await ctx.send("Uhm. This to-do doesn't exist.")
+        await self.bot.db.execute("DELETE FROM todos WHERE to_do_id = ?",(to_do_exists[0],))
+        await self.bot.db.commit()
+        return await ctx.send(f"Deleted to-do {to_do}.")
+    
+    @todo.command()
+    async def edit(self,ctx,to_do,new_to_do):
+        to_do_list_id = await self._get_author_to_do_list_id(ctx)
+        if not to_do_list_id:
+            return await ctx.send("You didn't setup a to-do list yet ! Create one using `?todo create <your first to do>` or simply `?todo create`.")
+        to_do_exists = await self._todo_exists(to_do_list_id[0],to_do)
+        if not to_do_exists:
+            return await ctx.send("Uhm. This to-do doesn't exist.")
+        await self.bot.db.execute("UPDATE todos SET to_do = ? WHERE to_do_id = ?",(new_to_do,to_do_exists[0]))
+        await self.bot.db.commit()
+        return await ctx.send(f"Successfully edited your to-do !")
+    
+    @todo.command()
+    async def pending(self,ctx):
+        sql = "SELECT to_do,importance FROM todos WHERE list_id = (SELECT list_id FROM todolists WHERE member_id = ? AND guild_id = ?)  AND done = 0 ORDER BY importance ASC;"
+        l = []
+        async with self.bot.db.execute(sql,(ctx.author.id,ctx.guild.id)) as cursor:
+            async for row in cursor:
+                l.append(f"{row[1]}. {row[0]} : \U0000274c")
+        if len(l) == 0:
+            l = ["No pending to-dos !"]
+        em = discord.Embed(title=f"{ctx.author}'s to do list (pending only).",color=0xaaffaa,timestamp=datetime.datetime.utcnow())
+        joined = "\n".join(l)
+        em.add_field(name="\u2800",value=f"```md\n{joined}\n```")
+        return await ctx.send(embed=em)
+    
+    @todo.command()
+    async def importance(self,ctx,n:int,*,to_do):
+        to_do_list_id = await self._get_author_to_do_list_id(ctx)
+        if not to_do_list_id:
+            return await ctx.send("You didn't setup a to-do list yet ! Create one using `?todo create <your first to do>` or simply `?todo create`.")
+        to_do_exists = await self._todo_exists(to_do_list_id[0],to_do)
+        if not to_do_exists:
+            return await ctx.send("Uhm. This to-do doesn't exist.")
+        await self.bot.db.execute("UPDATE todos SET importance = ? WHERE to_do_id = ?",(n,to_do_exists[0]))
+        await self.bot.db.commit()
+        return await ctx.send(f"Changed importance of to-do {n}.")
 
 def setup(bot):
     bot.add_cog(Reminder(bot))
