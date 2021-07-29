8b0c0c8d519301b54146e5360f987bc96676328b
Esteban <estebanmandjukowski@gmail.com>
2021-07-29 12:50:25 +0200

    Playlists are now functional, working on changing song's position, removing them etc

--- cogs/music.py
+++ cogs/music.py
@@ -1,8 +1,10 @@
+from _typeshed import Self
+from asyncio.base_events import _SendfileFallbackProtocol
 import discord
 from discord.ext import commands
 import asyncio
 from async_timeout import timeout
-from functools import partial
+from functools import cmp_to_key, partial
 from youtube_dl import YoutubeDL
 
 __all__ = ('NoVoiceClient',"NotSameVoiceChannel","AuthorIsNotInVoiceChannel")
@@ -49,14 +51,15 @@ class YTDLSource(discord.PCMVolumeTransformer):
         return self.__getattribute__(item)
 
     @classmethod
-    async def create_source(cls, ctx, search: str, *, loop, download=False):
+    async def create_source(cls, ctx, search: str, *, loop, download=False,playlist=False):
         loop = loop or asyncio.get_event_loop()
         to_run = partial(ytdl.extract_info, url=search, download=download)
         data = await loop.run_in_executor(None, to_run)
         if 'entries' in data:
             # take first item from a playlist
             data = data['entries'][0]
-        await ctx.send(f'Added `{data["title"]}` to the queue.', delete_after=15)
+        if not playlist:
+            await ctx.send(f'Added `{data["title"]}` to the queue.', delete_after=15)
         if download:
             source = ytdl.prepare_filename(data)
         else:
@@ -123,6 +126,21 @@ class Music(commands.Cog):
             result = await cursor.fetchone()
         return result
     
+    @commands.Cog.listener()
+    async def on_member_remove(self,member):
+        await self.bot.db.execute("UPDATE playlists SET creator_id = ? WHERE creator_id = ?",(None,member.id))
+        await self.bot.db.commit()
+
+    async def _count_songs_from_playlist(self,playlist_id):
+        async with self.bot.db.execute("SELECT COUNT(*) FROM songs WHERE playlist_id = ?",(playlist_id,)) as cursor:
+            result = await cursor.fetchone()
+        return result[0] or "0"
+
+    async def _get_playlist_creator_id(self,playlist_id):
+        async with self.bot.db.execute("SELECT creator_id FROM playlists WHERE playlist_id = ?",(playlist_id,)) as cursor:
+            result = await cursor.fetchone()
+        return result
+
     def register_songs(self,songs):
         songs_list = []
         for song in songs:
@@ -159,21 +177,21 @@ class Music(commands.Cog):
                 raise AuthorIsNotInVoiceChannel("Either join a voice channel or specify one !")
         if ctx.voice_client:
             if ctx.voice_client.channel.id == channel.id:
-                return #We are already connected to this voice channel.
+                return await ctx.send(f"Already connected to {ctx.voice_client.channel.mention}")#We are already connected to this voice channel.
             else:
                 try:
                     await ctx.voice_client.move_to(channel)
                 except asyncio.TimeoutError:
                     return await ctx.send(f"Connection to {channel.mention} somehow failed.")
                 else:
-                    return await ctx.send(f"Now connected to {channel.mention}.",delete_after=15)
+                    return await ctx.send(f"Now connected to {channel.mention}.",delete_after=10)
         else:
             try:
                 await channel.connect()
             except asyncio.TimeoutError:
                     return await ctx.send(f"Connection to {channel.mention} somehow failed.")
             else:
-                return await ctx.send(f"Now connected to {channel.mention}.",delete_after=15)
+                return await ctx.send(f"Now connected to {channel.mention}.",delete_after=10)
 
     @commands.group(aliases=["sing"])
     async def play(self,ctx,*,song):
@@ -190,6 +208,21 @@ class Music(commands.Cog):
         source = await YTDLSource.create_source(ctx,song,loop=self.bot.loop,download=False)
         await player.queue.put(source)
     
+    async def play_playlist(self,ctx,songs_list):
+        await ctx.trigger_typing()
+        if not ctx.voice_client:
+            await ctx.invoke(self.join)
+        try:
+            if ctx.voice_client.channel != ctx.author.voice.channel:
+                raise NotSameVoiceChannel("You must be in my voice channel to add a song to the queue !")
+        except AttributeError: #Author isn't in a voice channel
+            raise AuthorIsNotInVoiceChannel("You're not in a voice channel (and to be even more accurate, the voice channel I'm connected to), so you can't use this command.")
+        player = self.get_music_player(ctx)
+        for song in songs_list:
+            source = await YTDLSource.create_source(ctx,song[0],loop=self.bot.loop,download=False,playlist=True)
+            await player.queue.put(source)
+        return True
+
     @commands.command()
     async def stop(self,ctx):
         await self.cleanup(ctx.guild)
@@ -206,8 +239,8 @@ class Music(commands.Cog):
     async def skip(self,ctx):
         if not ctx.voice_client.is_playing(): #Self explanatory
             return await ctx.send("I'm not playing anything.")
-        player = self.get_music_player(ctx)
-        if player.queue.empty():
+        player = self.get_music_player(ctx) #Music player for ctx.guild
+        if player.queue.empty(): #Nothing to skip if queue is empty
             return await ctx.send("Current queue is empty.")
         voice_client_channel = ctx.voice_client.channel
         voice_channel_members = [member for member in voice_client_channel.members if not member.bot] #Gets every member in the bot's voice channel
@@ -215,20 +248,21 @@ class Music(commands.Cog):
             ctx.voice_client.stop()
             return await ctx.send("Song skipped.")
         formatted_choices = "\n".join([f" {self.white_check} Skip, this song is trash.",f"{self.red_cross} This song is dope."])
-        em = discord.Embed(title=f"Vote for skipping ! (ends in 10 seconds)",color=0x00ffbb,description=formatted_choices)
+        em = discord.Embed(title=f"Vote for skipping ! (ends in 10 seconds)",color=0x00ffbb,description=formatted_choices) #Vote for skipping.
+        em.set_footer(text="Only people that are in the voice channel in 10 seconds will see their votes counted.")
         poll = await ctx.send(embed=em)
         reactions_list = [self.white_check,self.red_cross]
         for i in reactions_list:
             await poll.add_reaction(i)
-        await asyncio.sleep(10)
-        r = discord.utils.get(self.bot.cached_messages,id=poll.id) or await ctx.channel.fetch_message(poll.id)
-        if r is None:
-            return await ctx.send("An error occured. Please restart the poll.")
+        await asyncio.sleep(10) #Wait until the vote ends
+        r = discord.utils.get(self.bot.cached_messages,id=poll.id) or await ctx.channel.fetch_message(poll.id) #In case message cannot be found in the bot's cache
+        if r is None: #Vote message not in cache neither could be fetched from the channel (most likely was deleteds)
+            return await ctx.send("An error occured. Please restart the poll. (the vote message was most likely deleted)")
         reactions_count = []
         for reaction in r.reactions:
             try:
                 reactions_count.append((reaction.emoji,len([member for member in await reaction.users().flatten() if not member.bot and member.voice.channel == ctx.voice_client.channel])))
-            except AttributeError: #Member.voice is None, so member.voice.channel raises this error
+            except AttributeError: #Member.voice is None, so member.voice.channel raises this error. Means someone who voted wasn't in the voice channel at the time.
                 continue
         maxi = max(reactions_count,key=lambda item:item[1])
         if maxi[1] == 0:
@@ -275,39 +309,43 @@ class Music(commands.Cog):
         player.volume = vol/100
         return await ctx.send(f"Volume set to {vol}.")
 
-    @commands.group(aliases=["playlists"],invoke_without_command=True)
-    async def playlist(self,ctx,*,playlist_name=None):
-        if ctx.invoked_subcommand is None:
-            if playlist_name is None:
-                playlists_list = []
-                em = discord.Embed(title=f"{ctx.guild}'s playlists !",color=discord.Colour.blurple())
-                async with self.bot.db.execute("SELECT playlist_id,playlist_name FROM playlists WHERE guild_id = ?",(ctx.guild.id,)) as cursor:
-                    async for row in cursor:
-                        playlist_id,playlist_name = row
-                        async with self.bot.db.execute("SELECT COUNT(*) FROM songs WHERE playlist_id = ?",(playlist_id,)) as songs:
-                            number_of_songs = await songs.fetchone()
-                        playlists_list.append(f"`- {playlist_name} ({number_of_songs[0]} songs).`")
-                em.description = "\n".join(playlists_list)
-                return await ctx.send(embed=em)
-            else:
-                playlist_exists = await self._playlist_exists(ctx.guild.id,playlist_name)
-                if playlist_exists is None:
-                    return await ctx.send(f"No playlist named '{playlist_name}' was found.")
-                playlist_id = playlist_exists[0]
-                sql = "SELECT song_url,song_name FROM songs WHERE playlist_id = ? ORDER BY song_position ASC;"
-                async with self.bot.db.execute(sql,(playlist_id,)) as cursor:
-                    playlist_songs = [f"- [`{song[1]}`]({song[0]})" for song in await cursor.fetchall()]
-                em = discord.Embed(title=f"{playlist_name} playlist - {len(playlist_songs)} songs.",description="\n".join(playlist_songs))
-                return await ctx.send(embed=em)
+    @commands.command()
+    async def playlists(self,ctx):
+        playlists_list = []
+        em = discord.Embed(title=f"{ctx.guild}'s playlists !",color=discord.Colour.blurple())
+        async with self.bot.db.execute("SELECT playlist_id,playlist_name FROM playlists WHERE guild_id = ?",(ctx.guild.id,)) as cursor:
+            async for row in cursor:
+                playlist_id,playlist_name = row
+                number_of_songs = await self._count_songs_from_playlist(playlist_id)
+                playlists_list.append(f"`- {playlist_name} ({number_of_songs[0]} songs).`")
+        if len(playlists_list) == 0:
+            em.description = "Woah. Emptiness."
+        else:
+            em.description = "\n".join(playlists_list)
+        return await ctx.send(embed=em)
+
+    @commands.group(invoke_without_command=True)
+    async def playlist(self,ctx,*,playlist_name):
+        playlist_exists = await self._playlist_exists(ctx.guild.id,playlist_name)
+        if playlist_exists is None:
+            return await ctx.send(f"No playlist named '{playlist_name}' was found.")
+        playlist_id = playlist_exists[0]
+        sql = "SELECT song_url,song_name FROM songs WHERE playlist_id = ? ORDER BY song_position ASC;"
+        async with self.bot.db.execute(sql,(playlist_id,)) as cursor:
+            playlist_songs = await cursor.fetchall()
+        songs_queued = await self.play_playlist(ctx,playlist_songs)
+        if songs_queued:
+            return await ctx.send(f"Let's go ! I will play the songs of `{playlist_name}` playlist.")
 
     @playlist.command(aliases=['new'])
     async def create(self,ctx,*args):
+        await ctx.message.edit(suppress=True)
         if len(args) == 0:
             return await ctx.send("You didn't give me a playlist name.")
         playlist_exists = await self._playlist_exists(ctx.guild.id,args[0])
         if playlist_exists:
             return await ctx.send(f"A playlist named `{args[0]}` already exists.")
-        playlist_created = await self.bot.db.execute_insert("INSERT INTO playlists(playlist_name,creator_id,guild_id) VALUES(?,?,?)",(args[0],ctx.author.id,ctx.guild.id))
+        playlist_created = await self.bot.db.execute_insert("INSERT INTO playlists(playlist_name,creator_id,guild_id,uses) VALUES(?,?,?,0)",(args[0],ctx.author.id,ctx.guild.id))
         await self.bot.db.commit()
         if len(args) > 1:
             nb_songs = len(args) - 1
@@ -318,7 +356,6 @@ class Music(commands.Cog):
             counter = 0
             for song in songs_list:
                 await self.bot.db.execute(sql,(playlist_created[0],song["webpage_url"],song["title"],counter))
-
                 counter += 1
             await self.bot.db.commit()
             return await ctx.send(f"Successfully created playlist `{args[0]}` ({nb_songs} songs).")
@@ -329,6 +366,9 @@ class Music(commands.Cog):
         playlist_exists = await self._playlist_exists(ctx.guild.id,playlist_name)
         if playlist_exists is None:
             return await ctx.send(f"A playlist named `{playlist_name}` doesn't exist.")
+        creator_id = await self._get_playlist_creator_id(playlist_exists[0])
+        if not(creator_id is None or creator_id[0] == ctx.author.id or ctx.author.guild_permissions.manage_guild):
+            return await ctx.send("This playlist has a owner. And you're not the owner. Neither do you have the `manage server` permissions. Thus you can't delete this playlist.")
         try:
             await ctx.send(f"Are you sure you want to delete the `{playlist_name}` playlist ?")
             confirm = await self.bot.wait_for("message",check=lambda m:m.author == ctx.author and m.channel == ctx.channel,timeout=15)
@@ -347,10 +387,30 @@ class Music(commands.Cog):
         playlist_exists = await self._playlist_exists(ctx.guild.id,playlist_name)
         if playlist_exists is None:
             return await ctx.send(f"A playlist named `{playlist_name}` doesn't exist.")
+        creator_id = await self._get_playlist_creator_id(playlist_exists[0])
+        if not(creator_id is None or creator_id[0] == ctx.author.id or ctx.author.guild_permissions.manage_guild):
+            return await ctx.send("This playlist has a owner. And you're not the owner. Neither do you have the `manage server` permissions. Thus you can't delete this playlist.")
         await self.bot.db.execute("UPDATE playlists SET playlist_name = ? WHERE playlist_id = ?",(new_playlist_name,playlist_exists[0]))
         await self.bot.db.commit()
         return await ctx.send(f"Playlist name changed : `{playlist_name}` -> `{new_playlist_name}`.")
 
+    @playlist.command()
+    async def info(self,ctx,*,playlist_name):
+        playlist_exists = await self._playlist_exists(ctx.guild.id,playlist_name)
+        if playlist_exists is None:
+            return await ctx.send(f"No playlist named '{playlist_name}' was found.")
+        playlist_id = playlist_exists[0]
+        async with self.bot.db.execute("SELECT creator_id,created_at,uses FROM playlists WHERE playlist_id = ?",(playlist_id,)) as cursor:
+            playlist_infos = await cursor.fetchone()
+        sql = "SELECT song_url,song_name FROM songs WHERE playlist_id = ? ORDER BY song_position ASC;"
+        async with self.bot.db.execute(sql,(playlist_id,)) as cursor:
+            playlist_songs = [f"- [`{song[1]}`]({song[0]})" for song in await cursor.fetchall()]
+        creator = ctx.guild.get_member(playlist_infos[0]) or "Creator left the server."
+        em = discord.Embed(title=f"'{playlist_name}' playlist - {len(playlist_songs)} songs.",description="\n".join(playlist_songs))
+        em.add_field(name="Created at :",value=playlist_infos[1])
+        em.add_field(name="Creator :",value=creator.mention)
+        em.add_field(name="Uses : ",value=playlist_infos[2])
+        return await ctx.send(embed=em)
         
     @pause.before_invoke
     @stop.before_invoke
